fc1bfa81f74e1a522f9c4832eca6434a68345ae4
Trevor Gale <gale.t@husky.neu.edu>
2017-04-26 17:55:48 -0400

    Linear regression in main.cc. It works!

---
+++ quark/io.h
@@ -0,0 +1,78 @@
+#ifndef QUARK_IO_H_
+#define QUARK_IO_H_
+
+#include <cstdlib>
+#include <fstream>
+#include <random>
+#include <sstream>
+#include <string>
+
+#include "quark/common.h"
+#include "quark/tensor.h"
+#include "quark/util/backend_util.h"
+
+namespace quark {
+
+/**
+ * @Loads a data from a text file into a Tensor. Each line of the input file is loaded as a row.
+ *
+ * This function expects the data to be stored as CSV, where each line represents a row in a
+ * matrix. Each line must contain the same number of data points.
+ */
+template <typename T, typename Backend>
+void LoadFromTextFile(const string file_name, Tensor<T, Backend>* t) {
+  std::ifstream f(file_name);
+  vector<T> data;
+
+  {
+    string first_line;
+    std::getline(f, first_line);
+    std::istringstream iss(first_line);
+    
+    for (std::string tmp; getline(iss, tmp, ',');) {
+      T val = strtof(tmp.c_str(), nullptr);
+      data.push_back(val);
+    }
+  }
+    
+  int64 cols = data.size();
+  QUARK_CHECK(cols > 0, "Could not read any values from the first line of " + file_name);
+
+  // read in the remaining lines
+  for (std::string line; getline(f, line);) {
+    std::istringstream iss(line);
+    int64 line_size = 0;
+    for (std::string str_val; getline(iss, str_val, ',');) {
+      T val = strtof(str_val.c_str(), nullptr);
+      data.push_back(val);
+      line_size++;
+    }
+    QUARK_ASSERT(line_size == cols,
+        "All lines in input file must contain the same number of data points");
+  }
+
+  // load the data into the tensor
+  vector<int64> dims = {int64(data.size()) / cols, cols};
+  t->Resize(dims);
+
+  CopyData(data.size(), data.data(), t->mutable_data());
+}
+
+/**
+ * @brief Fills the input Tensor with random values (between 0 and 1)
+ */
+template <typename T, typename Backend>
+void InitRandomTensor(vector<int64> shape, Tensor<T, Backend>* t) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> distr;
+  vector<T> data(Prod(shape), 0.0);
+
+  t->Resize(shape);  
+  for (auto& val : data) val = (T)distr(gen);
+  CopyData(data.size(), data.data(), t->mutable_data());
+}
+
+} // namespace quark
+
+#endif // QUARK_IO_H_
--- quark/main.cc
+++ quark/main.cc
@@ -1,19 +1,52 @@
 #include <iostream>
 
 #include "quark/compute_graph.h"
-#include "quark/cpu_backend.h"
-#include "quark/cuda_backend.h"
+#include "quark/io.h"
+#include "quark/ops/math_ops.h"
 #include "quark/ops/utility_ops.h"
 #include "quark/tensor.h"
+
 using namespace std;
 using namespace quark;
 
 int main() {
-  ComputeGraph<float> graph;
-  vector<float> data = {1, 2, 3, 4};
-  Tensor<float, CudaBackend> a({2, 2}, data);
-  Tensor<float, CudaBackend> b;
+  // Load the data and labels & initialize beta
+  Tensor<float, CudaBackend> data, labels, beta;
+  LoadFromTextFile("./dummy_data/small_train.data", &data);
+  LoadFromTextFile("./dummy_data/small_train.labels", &labels);
+  InitRandomTensor({data.shape()[1], 1}, &beta);
 
-  Copy(&graph, a, &b);
-  graph.Compile();
+  int num_samples = data.shape()[0];
+  int max_iter = 100;
+  float learning_rate = .01;
+  
+  cout << data.shape()[0] << " x " << data.shape()[1] << endl;
+  cout << labels.shape()[0] << " x " << labels.shape()[1] << endl;
+  cout << beta.shape()[0] << " x " << beta.shape()[1] << endl;
+  
+  // Intermediate results
+  Tensor<float, CudaBackend> xb, y_m_xb, loss, grad, new_beta;
+
+  // Build the graph
+  ComputeGraph<float> cg;
+
+  Matmul(&cg, 1.0f, false, data, false, beta, &xb);
+
+  Add(&cg, 1.0f, false, labels, -1.0f, false, xb, &y_m_xb);
+
+  Matmul(&cg, 1.0f / num_samples, true, y_m_xb, false, y_m_xb, &loss);
+
+  Matmul(&cg, -2.0f / num_samples, true, data, false, y_m_xb, &grad);
+
+  Add(&cg, 1.0f, false, beta, -learning_rate, false, grad, &new_beta);
+      
+  cg.Compile();
+
+  for (int i = 0; i < max_iter; ++i) {
+    cg.Execute();
+
+    cout << "loss: " << loss;
+    
+    beta.Copy(new_beta);
+  }
 }
